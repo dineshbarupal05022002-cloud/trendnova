30c0970eb73eb285f2a77fe37be10be0e41e24b2
dineshbarupal05022002-cloud <dineshbarupal05022002@gmail.com>
2026-08-04 16:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function addToCart(productName) {
   document.getElementById("cart-count").innerText = cartCount;
   alert(productName + " added to cart!");
 }
+
+let cart = [];
+
+function addToCart(productName) {
+  cart.push(productName);
+
+  document.getElementById("cart-count").innerText = cart.length;
+
+  alert(productName + " added to cart!");
+}
+
+function showCart() {
+  let cartBox = document.getElementById("cart-items");
+
+  if (cart.length === 0) {
+    cartBox.innerHTML = "<h3>Your Cart is Empty!</h3>";
+    return;
+  }
+
+  let html = "<h2>🛒 Your Cart</h2><ul>";
+
+  cart.forEach(function(item) {
+    html += "<li>" + item + "</li>";
+  });
+
+  html += "</ul>";
+
+  cartBox.innerHTML = html;
+}
